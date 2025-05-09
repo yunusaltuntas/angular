5d2b43d89ed5f0138f2870cf1131ccd45a3188e8
yunusaltuntas <altuntasyunus50@gmail.com>
2025-05-09 14:47:37 +0300

    add app

---
+++ src/app/employee.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {environment} from "../enivroments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Employee} from "./employee";
+import {Observable} from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class EmployeeService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient){}
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+}
